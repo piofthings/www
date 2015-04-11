61299469a88471f37d35b2539c2df8c24262e1fd
Sumit Maitra <sumitkm@gmail.com>
2015-04-11 22:06:00 +0100

    Base code. Only home page works! Registration disable till we get SSL in place.

---
+++ piofthings.hub/PiotHub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using PiOfThings.GpioUtils;
+
+namespace piofthings.hub
+{
+    [HubName("PiotHub")]
+    public class PiotHub : Hub
+    {
+        public bool Handshake()
+        {
+            return true;
+        }
+
+        public void SwitchOn(GpioId gpioPinId)
+        {
+            Console.WriteLine("Switching ON - " + gpioPinId.ToString("D"));
+            Clients.Others.switchOn(gpioPinId);
+        }
+
+        public void switchOff(GpioId gpioPinId)
+        {
+            Console.WriteLine("Switching OFF - " + gpioPinId.ToString("D"));
+
+            Clients.Others.switchOff(gpioPinId);
+        }
+
+    }
+}
